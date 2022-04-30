9456f8506110e00cbcf149fb6c68c0911e1d9fd4
Alexander Shavrov <alexander.schawrow@gmail.com>
2022-04-30 18:31:50 +0300

    Added installing configuration

---
+++ basic/installing/include/hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+struct Hello {
+    void print();
+};
+
+#endif // HELLO_H
---
+++ basic/installing/src/hello.cpp
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+#include <iostream>
+
+void Hello::print()
+{
+    std::cout << "This is install!\n";
+}
